3acfec26dfb9cf6ba6fde3c33879e4695bd744b7
Duy Nguyen <duy.nguyen@rockship.co>
2017-10-02 14:40:25 +0700

    clone

--- index.js
+++ index.js
@@ -12,10 +12,31 @@ const server = require("http").createServer(app);
 
 const io = require("socket.io")(server);
 
-server.listen(5000,() => console.log('server start'));
+server.listen(process.env.PORT || 5000,() => console.log('server start'));
 
-io.on("connection",(socket)=>{
-    console.log("co nguoi ket noi");
+const arrUserInfo = [];
+
+
+// catch the event connection 
+io.on("connection",(socket) => {
+    socket.on('Active_User',(user) => { 
+        socket.peerID = user.peerID;
+
+        // validate username
+        const isExist = arrUserInfo.some((e) => e.Ten === user.Ten);
+        if(isExist) return socket.emit('Already_exists');
+        arrUserInfo.push(user);
+        socket.emit("Active_User",arrUserInfo);
+        socket.broadcast.emit('New_User',user); 
+    });
+    // catch the event disconnect
+    socket.on("disconnect", () => {
+        const index = arrUserInfo.findIndex((user) => user.peerID === socket.peerID);
+        arrUserInfo.splice(index , 1);
+        io.emit('User_disconnect',socket.peerID);
+    });
+
 });
+
 
 app.get('/', (req,res) => res.render('home'));
--- src/app.js
+++ src/app.js
@@ -10,48 +10,82 @@ const playVideo = require('./playVideo');
 
 const socket = io('http://localhost:5000');
 
+
+$('#dChat').hide();
+
+// display username 
+socket.on('Active_User',(arrUserInfo) => {
+    // event hiding show
+    $('#dChat').show();
+    $('#dDK').hide();
+
+    // take user information and display screen
+    arrUserInfo.forEach((user) => {
+        const {Ten , peerID} = user;
+        $('#ulUser').append(`<li id="${peerID}" class="list-group-item"> ${Ten} </li>`);
+    });
+
+    // everyone can connection and 
+    socket.on('New_User',(user) => {
+        const {Ten , peerID} = user;
+        $('#ulUser').append(`<li id="${peerID}" class="list-group-item"> ${Ten} </li>`);
+    });
+    
+    // User disconnect
+    socket.on('User_disconnect',(peerID) => {
+        $(`#${peerID}`).remove();
+    })
+});
+
+socket.on('Already_exists', () => alert('Tên Người Dùng Đã Tồn Tại,Vui Lòng Chọn UserName Khác'));
+
+
+
+
+// event function when an extra person wants to online
 function getPeer(){
     const id = uid(10);
     $('#peer-id').append(id);
+    $('#btnSignUp').click(() => {
+        const username = $('#txtUsername').val();
+        socket.emit('Active_User',{Ten :username, peerID:id});
+    });
     return id;
 }
 
-var customConfig;
-
-// $.ajax({
-// url: "https://global.xirsys.net",
-// data: {
-//   ident: "duynguyenbut1996",
-//   secret: "e44c5edc-a1e0-11e7-8ff9-07de7e02ee98",
-//   channel: "MyFirstApp",
-//   secure: 1
-// },
-// success: function (data, status) {
-//   // data.v is where the iceServers object lives
-//   customConfig = data.v;
-//   console.log(customConfig);
-// },
-// async: false
-// });
-
+// key peerjs
 const connectionObj = {key: '3jqb18u6ub2fn7b9'};
-
 
 const peer = Peer(getPeer(), connectionObj);
 
-$('#btnConnect').click(() => {
-    const Yourid =  $('#tokenID').val();
-    openCamera(stream => {
-        playVideo(stream, 'loadvideo');
-        const call = peer.call(Yourid , stream);
-        call.on('stream',remoteStream => playVideo(remoteStream,'loadvideoSc'));
-    });
-});
+// caller
+// $('#btnConnect').click(() => {
+//     const Yourid =  $('#tokenID').val();
+//     openCamera(stream => {
+//         playVideo(stream, 'loadvideo');
+//         const call = peer.call(Yourid , stream);
+//         call.on('stream',remoteStream => playVideo(remoteStream,'loadvideoSc'));
+//     });
+// });
 
+// people listen
 peer.on('call',(call) =>{
     openCamera(stream => {
         playVideo(stream, 'loadvideo');
         call.answer(stream);
+        call.on('stream',remoteStream => playVideo(remoteStream,'loadvideoSc'));
+    });
+});
+
+
+
+// Click Caller update
+
+$('#ulUser').on('click','li',function(){
+    const ids = $(this).attr('id');
+    openCamera(stream => {
+        playVideo(stream, 'loadvideo');
+        const call = peer.call(ids , stream);
         call.on('stream',remoteStream => playVideo(remoteStream,'loadvideoSc'));
     });
 });
